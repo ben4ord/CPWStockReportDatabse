ffc337d62a364f41c9133490ed87b9182a644e47
Braxton Walk <148299966+BraxtonWalk@users.noreply.github.com>
2024-12-14 18:50:11 -0700

    Add files via upload

--- Query_Statements.sql
+++ Query_Statements.sql
@@ -1,12 +1,17 @@
 #SINGLE QUERY
 #1. What vehicles haven’t been serviced in the last 12 months? 
+SELECT type, service_date, license_number, IF(datediff(current_date(), service_date) >= 365, "Needs Service", "No Service Needed") AS "Needs Service?"
+FROM vehicle;
 
 #2. Which employees have been with the company for over 2 years?
+SELECT concat(first_name, " ", last_name) AS "Name", hire_date, type
+FROM employee
+WHERE datediff(current_date(), hire_date) >= 730;
 
 #3. Which fish are not native to North America
 SELECT species, size, nativity
 FROM fish
-WHERE nativity LIKE "%North America%";
+WHERE nativity NOT LIKE "%North America%";
 
 #4. Which body of water has the greatest depth?
 SELECT name, depth_ft
@@ -15,14 +20,14 @@ ORDER BY depth_ft DESC
 LIMIT 1;
 
 #5. Which fisheries breed rainbow trout? 
-SELECT*
+SELECT name
 FROM fishery
 WHERE purpose LIKE "%rainbow trout%";
 
 
 #MULTITABLE QUERIES
 #1. Which body of water gets the greatest number of fish deposited into it?
-SELECT body_of_water.name, SUM(fish_fishery.quantity) AS total_quantity
+SELECT body_of_water.name, SUM(fish_fishery.quantity)*100 AS total_quantity
 FROM fish_fishery
 JOIN fishery
 ON fish_fishery.fishery_ID = fishery.fishery_ID
@@ -80,4 +85,57 @@ ON fishery_body_of_water.fishery_ID = fishery.fishery_ID
 WHERE fishery.name = "Roaring Judy Hatchery";
 
 
+#STORED PROCEDURES
+#1. Which types of fish does a specific employee work with? 
+DROP PROCEDURE IF EXISTS employeeFish;
 
+DELIMITER //
+CREATE PROCEDURE employeeFish(
+	IN employee_first_name VARCHAR(25),
+    IN employee_last_name VARCHAR(25)
+)
+BEGIN
+	SELECT species
+	FROM fish
+	WHERE fish_ID IN (
+		SELECT fish_ID
+		FROM fish_fishery
+		WHERE fishery_ID IN (
+			SELECT fishery_ID
+			FROM fishery
+			WHERE fishery_ID IN (
+				SELECT fishery_ID
+				FROM fishery_worker
+				WHERE employee_ID IN (
+					SELECT employee_ID
+					FROM employee
+					WHERE first_name = employee_first_name AND last_name = employee_last_name))));
+END//
+DELIMITER ;
+
+
+#2. Which types of fish are being supplied to a specific body of water? 
+DROP PROCEDURE IF EXISTS bodyOfWaterFish;
+
+DELIMITER //
+CREATE PROCEDURE bodyOfWaterFish(
+	IN body_of_water_name VARCHAR(45)
+)
+BEGIN
+	SELECT species
+	FROM fish
+	WHERE fish_ID IN (
+		SELECT fish_ID
+		FROM fish_fishery
+		WHERE fishery_ID IN (
+			SELECT fishery_ID
+			FROM fishery
+			WHERE fishery_ID IN (
+				SELECT fishery_ID
+				FROM fishery_body_of_water
+				WHERE body_of_water_ID IN (
+					SELECT body_of_water_ID
+					FROM body_of_water
+					WHERE body_of_water.name = body_of_water_name))));
+END//
+DELIMITER ;
